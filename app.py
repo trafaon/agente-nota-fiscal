671cf025f24a23121890d8e1495edfeac5a5f11f
Rafa <95707274+trafaon@users.noreply.github.com>
2025-06-13 18:10:21 -0300

    Update app.py
    ajustado o llama_index_tools

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from llama_index.core import Settings, VectorStoreIndex
 from llama_index.llms.groq import Groq
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.readers.file import CSVReader
+from llama_index.tools.query_engine_tool import QueryEngineTool
 import zipfile
 
 # Função para extrair o ZIP (executa uma única vez)
